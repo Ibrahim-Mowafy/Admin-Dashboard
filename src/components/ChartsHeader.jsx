7c1fa838c9f659419f6ca55afd28c02fc23e9277
ibrahim <ibrahim.mowafy10@gmail.com>
2022-06-11 16:41:36 +0200

    Adding folders & files structure

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartHeader = () => {
+  return <div>ChartHeader</div>;
+};
+
+export default ChartHeader;
---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ECommerce = () => {
+  return <div>ECommerce</div>;
+};
+
+export default ECommerce;
