eca6d09f4d5c0264d8e47a399bad1b4571fc6504
ibrahim <ibrahim.mowafy10@gmail.com>
2022-06-25 23:06:44 +0200

    Adding classes to tailwind safe list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  safelist: ["text-green-600", "text-red-600"],
+  safelist: ["text-green-600", "text-red-600", "block", "hidden"],
   darkMode: "class",
   theme: {
     fontFamily: {
